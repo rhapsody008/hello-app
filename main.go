00d739f12bea31ba1291a762e3680cca30b43c52
Zhou Yi <82878185+rhapsody008@users.noreply.github.com>
2026-04-28 08:20:09 +0800

    Comment out all code in main.go

--- main.go
+++ main.go
@@ -1,38 +1,38 @@
-package main
+// package main
  
-import (
-	"fmt"
-	"log"
-	"net/http"
-	"os"
-)
+// import (
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// 	"os"
+// )
  
-func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "App is up and running!")
+// func handler(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintln(w, "App is up and running!")
  
-	clusterName := os.Getenv("CLUSTER_NAME")
-	if clusterName == "" {
-		clusterName = "unknown"
-	}
-	fmt.Fprintf(w, "Hello from %s!\n", clusterName)
-}
+// 	clusterName := os.Getenv("CLUSTER_NAME")
+// 	if clusterName == "" {
+// 		clusterName = "unknown"
+// 	}
+// 	fmt.Fprintf(w, "Hello from %s!\n", clusterName)
+// }
  
-func healthz(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "ok")
-}
+// func healthz(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintln(w, "ok")
+// }
  
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/hello", handler)
-	mux.HandleFunc("/healthz", healthz)
+// func main() {
+// 	mux := http.NewServeMux()
+// 	mux.HandleFunc("/hello", handler)
+// 	mux.HandleFunc("/healthz", healthz)
  
-	addr := ":8080"
-	if v := os.Getenv("PORT"); v != "" {
-		addr = ":" + v
-	}
+// 	addr := ":8080"
+// 	if v := os.Getenv("PORT"); v != "" {
+// 		addr = ":" + v
+// 	}
  
-	log.Printf("App is up and running! Listening on %s", addr)
-	if err := http.ListenAndServe(addr, mux); err != nil {
-		log.Fatalf("server failed: %v", err)
-	}
-}
+// 	log.Printf("App is up and running! Listening on %s", addr)
+// 	if err := http.ListenAndServe(addr, mux); err != nil {
+// 		log.Fatalf("server failed: %v", err)
+// 	}
+// }
